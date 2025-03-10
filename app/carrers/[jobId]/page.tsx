7a89c09560e6a4b0bbe490d61ac7a444a4465610
Shivansh <shivanshsaxena2910@gmail.com>
2025-03-11 00:09:57 +0530

    fix

--- app/carrers/[jobId]/page.tsx
+++ app/carrers/[jobId]/page.tsx
@@ -3,7 +3,9 @@ import JobApplicationForm from "../components/jobForm";
 const JobForm = () => {
   return (
     <div className="flex flex-col items-center justify-center pt-20 md:pt-10 gap-10 mx-10 md:mx-40">
-      <div className="text-4xl font-semibold text-[#b00d07]">Job Application Form</div>
+      <div className="text-4xl font-semibold text-[#b00d07]">
+        Job Application Form
+      </div>
       <JobApplicationForm />
     </div>
   );
